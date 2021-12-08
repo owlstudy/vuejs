b2d9f6c01c96670cdf53d29010c686860820df94
hyunjinee <leehj0110@kakao.com>
2021-12-08 23:12:52 +0900

    feat: home page

--- workout-tracker/src/views/Home.vue
+++ workout-tracker/src/views/Home.vue
@@ -1,19 +1,41 @@
 <template>
-  <div></div>
+  <div v-if="dataLoaded" class="container mt-10 px-4">
+    <div v-if="data.length === 0" class="w-full flex flex-col items-center">
+      <h1 class="text-2xl">Looks empty here...</h1>
+      <!-- <router-link :to="{ name: 'Create' }"> Create Workout </router-link> -->
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue"
+import { supabase } from "../supabase/init"
 export default {
-  name: "Home",
+  name: "home",
   components: {},
   setup() {
     // Create data / vars
-
+    const data = ref([])
+    const dataLoaded = ref(null)
     // Get data
+    const getData = async () => {
+      try {
+        const { data: workouts, error } = await supabase
+          .from("workouts")
+          .select("*")
+        // console.log(data)
+        if (error) throw error
+        data.value = workouts
+        dataLoaded.value = true
+        console.log(data.value, "data value")
+      } catch (error) {
+        console.warn(error.message)
+      }
+    }
 
     // Run data function
-
-    return {};
+    getData()
+    return { data, dataLoaded }
   },
-};
+}
 </script>
